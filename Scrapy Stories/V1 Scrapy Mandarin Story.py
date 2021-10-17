2d8bdd229a2e74c6066402a7ef3dd616a9bfcbbb
Ender Jiang <ender@hiverlab.com>
2021-10-18 00:14:41 +0800

    update scrapy story

--- Scrapy Stories/V1 Scrapy Mandarin Story.py
+++ Scrapy Stories/V1 Scrapy Mandarin Story.py
@@ -1,31 +1,14 @@
-# from gevent import monkey
-# monkey.patch_all()
 from bs4 import BeautifulSoup
-import gevent,requests, bs4, selenium, gspread, csv, time
-# from gevent.queue import Queue
-from bs4 import BeautifulSoup
-import requests
+import requests,os
 from bs4.dammit import EncodingDetector
-from fpdf import FPDF 
-
-rawstory=[]
-titletext=''
-def makelocalpdf():
-    pdf = FPDF() 
-    pdf.add_page() 
-    pdf.set_font("Arial", size = 15) 
-    f = rawstory
-    for x in f: 
-        pdf.cell(200, 10, txt = x, ln = 1, align = 'C') 
-    pdf.output('/Users/ender/Dropbox/Python/Test/readsmart/Scrapy content/'+str(111)+'.pdf')
-    #pdf.output('/Users/ender/Dropbox/Python/Test/readsmart/Scrapy content/'+str(titletext)+'.pdf')
-
-material_url='http://www.qigushi.com/shuiqian/1024.html'
-
-#http://www.qigushi.com/shuiqian/index_14.html
+# set root folder as base
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 urllist=[]
 material_url='http://www.qigushi.com/shuiqian/index_3.html'
 def service():
+    global fulltitle
+    titletext=''
+    rawstory=''
     headers = {
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'
         }
@@ -45,19 +28,17 @@ def service():
         title=soup.find_all(class_='breadcrumb')
         for m in title:
             titletext=m.get_text()
+            print('continue....')
             #titlefinal=titletext.partition('\n')[0]
-            print(titletext)
-            time.sleep(1)
+            #time.sleep(1)
         content=soup.find_all(class_='article_content')
         for z in content:
             rawstory=z.get_text()
-            print(rawstory)
-            #print(z.get_text())
         urllist.append(linklist)
-        makelocalpdf()
+        print(titletext)
+        print(rawstory)
 
 for j in range(2,15):
     material_url='http://www.qigushi.com/shuiqian/index_'+str(j)+'.html'
     #print(j)
     service()
-print(urllist)
